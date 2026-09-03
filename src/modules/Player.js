87063dee9839fc8f7f0f0ba650a84b9effcd1508
Marcus W <marcus_w_97@yahoo.com>
2026-09-03 22:29:08 +0100

    Add functionality to the createPlayer module including attack and generate moves for ai

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -1,6 +1,41 @@
-const createPlayer = (name) => {
-    return {
-        name : name,
-        ships : [],
-    };
+export const createPlayer = (name, isHuman = true) => {
+    const playerName = name; 
+    let hasWon = false;
+    const isHuman = false;
+    const allMoves = []
+
+    const attack = (gameboard, row, col) => {
+        return gameboard.receiveAttack(row, col)
+    }
+
+    const generateAllMoves = (row = 10, col = 10) => {
+        for (let i = 0; i< row; i++) {
+            for (let x = 0; x < col; x ++) {
+                allMoves.push([i, x])
+            }
+        }
+    }
+
+    if (isHuman === false) {
+        generateAllMoves()
+    }
+
+    const movePicker = () => {
+         return Math.floor(Math.random()*allMoves.length)
+    }
+
+    const computerMove = () => {
+        const randomIndex = movePicker();
+        const lastIndex = allMoves.length - 1;
+
+        [allMoves[randomIndex], allMoves[lastIndex]] = [allMoves[lastIndex], allMoves[randomIndex]]
+        return allMoves.pop()
+    }
+
+    return { 
+        playerName,
+        hasWon,
+        attack,
+        computerMove
+    }
 }
