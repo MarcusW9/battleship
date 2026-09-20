7cbbad9abbfcd5ad0f15c13619fbfc9478290077
Marcus W <marcus_w_97@yahoo.com>
2026-09-20 12:33:01 +0100

    Add name persistence on index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import { createGameController } from "./modules/Controller.js";
 import { displayController } from "./modules/displayController.js";
 
+
+let admiralName = ""
+
 // Pass the callback into displayController to wait for user to enter name
 displayController.init({
     onBeginGame : (typedName) => {
         // This code will run in index.js after being handed back by displayController
 
-        // 1. Initilialise the game controller with the correct name
+        // 1. Store the name
+        admiralName = typedName
+        // 2. Initilialise the game controller with the correct name
         const gameController = createGameController(typedName)
-        // 2. Pass it into displayController
+        // 3. Pass it into displayController
         displayController.resetGame(gameController)
     }
 });
